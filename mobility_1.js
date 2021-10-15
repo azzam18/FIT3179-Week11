4bd27dbc222e5a2b02df931af302937cbd2eaf34
azzam18 <92562960+azzam18@users.noreply.github.com>
2021-10-15 13:13:10 +0700

    Add files via upload

---
+++ mobility_1.js
@@ -0,0 +1,11 @@
+var vg1 = "mobility_1.json";
+
+vegaEmbed('#vis1', vg1).then(function(result){
+
+}).catch(console.error);
+
+var vg2 = "mobility_2.json";
+
+vegaEmbed('#vis2', vg2).then(function(result){
+
+}).catch(console.error);
